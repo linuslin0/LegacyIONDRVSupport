ab5c106611a41e6efc9ee52125593657289c867f
Linus L <delta0lin@gmail.com>
2018-07-20 12:52:59 -0400

    Use IOLog for older SDK.

--- LegacyIONDRVSupport/LegacyIONDRVSupport.hpp
+++ LegacyIONDRVSupport/LegacyIONDRVSupport.hpp
@@ -10,7 +10,7 @@
 
 #include <IOKit/IOService.h>
 #include <IOKit/ndrvsupport/IONDRVLibraries.h>
-#include <os/log.h>
+#include <Availability.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -29,11 +29,23 @@ IOReturn IOGetHardwareClamshellState( IOOptionBits * result );
 #define XSTR(a) STR(a)
 #define STR(a) #a
 
+#ifdef __MAC_10_12
+#include <os/log.h>
+// Simple wrapper for new os_log API, available in macOS 10.12 SDK and later
 #define ilog(fmt, args...) os_log_with_type(OS_LOG_DEFAULT, OS_LOG_TYPE_DEFAULT, XSTR(PRODUCT_NAME) ": " fmt, ##args)
 #define ilog_fault(fmt, args...) os_log_with_type(OS_LOG_DEFAULT, OS_LOG_TYPE_FAULT, XSTR(PRODUCT_NAME) ": " fmt, ##args)
 #define ilog_error(fmt, args...) os_log_with_type(OS_LOG_DEFAULT, OS_LOG_TYPE_ERROR, XSTR(PRODUCT_NAME) ": " fmt, ##args)
 #define ilog_debug(fmt, args...) os_log_with_type(OS_LOG_DEFAULT, OS_LOG_TYPE_DEBUG, XSTR(PRODUCT_NAME) ": " fmt, ##args)
 #define ilog_info(fmt, args...) os_log_with_type(OS_LOG_DEFAULT, OS_LOG_TYPE_INFO, XSTR(PRODUCT_NAME) ": " fmt, ##args)
+#else
+// Use IOLog for earlier SDK compatibility
+#include <IOKit/IOLib.h>
+#define ilog(fmt, args...) IOLog( XSTR(PRODUCT_NAME) ": [INFO] " fmt "\n", ##args )
+#define ilog_fault(fmt, args...) IOLog( XSTR(PRODUCT_NAME) ": [FAULT] " fmt "\n", ##args )
+#define ilog_error(fmt, args...) IOLog( XSTR(PRODUCT_NAME) ": [ERROR] " fmt "\n", ##args )
+#define ilog_debug(fmt, args...) IOLog( XSTR(PRODUCT_NAME) ": [DEBUG] " fmt "\n", ##args )
+#define ilog_debug(fmt, args...) IOLog( XSTR(PRODUCT_NAME) ": [INFO] " fmt "\n", ##args )
+#endif // __MAC_10_12
 
 class PRODUCT_NAME : public IOService
 {
